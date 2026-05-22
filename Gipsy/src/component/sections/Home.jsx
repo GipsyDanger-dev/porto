3b252e392369d52a0aabaf7e4c0cde9f04eba5ff
sharkjira99@gmail.com <sharkjira99@gmail.com>
2026-05-22 23:49:34 +0700

    fix: hide scroll indicator on mobile to prevent overlap

--- Gipsy/src/component/sections/Home.jsx
+++ Gipsy/src/component/sections/Home.jsx
@@ -60,7 +60,7 @@ export const Home = () => {
       </RevealOnScroll>
       
       {/* Scroll Down Indicator */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce z-10">
+      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce z-10 hidden md:block">
         <a href="#about" className="text-gray-500 hover:text-white transition-colors">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 14l-7 7m0 0l-7-7m7 7V3" />
