a009dbac901b5b415adb512aab2a062ad1e00c0a
sharkjira99@gmail.com <sharkjira99@gmail.com>
2026-06-16 14:42:07 +0700

    chore: remove 5 orphan components, 18 unused images, unused dep react-fast-marquee, unused imports

--- Gipsy/src/component/HeroScene.jsx
+++ Gipsy/src/component/HeroScene.jsx
@@ -1,7 +1,6 @@
 import { useRef, useMemo } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
-import { Float, MeshTransmissionMaterial } from '@react-three/drei';
-import * as THREE from 'three';
+import { Float } from '@react-three/drei';
 
 function GlowingTorus() {
   const meshRef = useRef();
